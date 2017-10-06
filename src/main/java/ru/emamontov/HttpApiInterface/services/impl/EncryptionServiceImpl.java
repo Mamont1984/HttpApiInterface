213f1e4fb81cfaeab88c8916a6702b2b7853f765
Mamont1984 <mamontovevgeny@mail.ru>
2017-10-06 17:09:55 +0300

    Upgrade password encryption function

--- src/main/java/ru/emamontov/HttpApiInterface/services/impl/EncryptionServiceImpl.java
+++ src/main/java/ru/emamontov/HttpApiInterface/services/impl/EncryptionServiceImpl.java
@@ -3,6 +3,8 @@ package ru.emamontov.HttpApiInterface.services.impl;
 import org.springframework.stereotype.Service;
 import ru.emamontov.HttpApiInterface.services.EncryptionService;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 @Service
@@ -12,7 +14,27 @@ public class EncryptionServiceImpl implements EncryptionService {
 
     @Override
     public String encrypt(String str) {
-        return String.valueOf(str.hashCode());
+            MessageDigest digest = null;
+            try {
+                digest = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+
+                //todo
+            }
+
+            final byte[] bytes = digest.digest(str.getBytes());
+
+            final StringBuilder hexString = new StringBuilder();
+            for (final byte b : bytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
     }
 
     @Override
