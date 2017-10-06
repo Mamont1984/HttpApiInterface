e38e7c60f2bb0aebdc398da2ce0232ce2b96d717
Mamont1984 <mamontovevgeny@mail.ru>
2017-10-06 17:54:42 +0300

    Upgrade password encryption function. Again.

--- src/main/java/ru/emamontov/HttpApiInterface/services/impl/EncryptionServiceImpl.java
+++ src/main/java/ru/emamontov/HttpApiInterface/services/impl/EncryptionServiceImpl.java
@@ -3,8 +3,10 @@ package ru.emamontov.HttpApiInterface.services.impl;
 import org.springframework.stereotype.Service;
 import ru.emamontov.HttpApiInterface.services.EncryptionService;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Random;
 
 @Service
@@ -14,27 +16,15 @@ public class EncryptionServiceImpl implements EncryptionService {
 
     @Override
     public String encrypt(String str) {
-            MessageDigest digest = null;
-            try {
-                digest = MessageDigest.getInstance("SHA-256");
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-
-                //todo
-            }
-
-            final byte[] bytes = digest.digest(str.getBytes());
-
-            final StringBuilder hexString = new StringBuilder();
-            for (final byte b : bytes) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            //todo
+        }
+        byte[] hash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+        String encoded = Base64.getEncoder().encodeToString(hash);
+        return encoded;
     }
 
     @Override
